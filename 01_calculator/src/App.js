d2937fa88af36eea30876bb87ad8153a144075a8
naveen <stupiddint@gmail.com>
2023-02-13 18:27:37 +0530

    logic part added

--- 01_calculator/src/App.js
+++ 01_calculator/src/App.js
@@ -1,7 +1,9 @@
 import { useReducer } from 'react';
 import './styles.css'
+import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
 
-const ACTIONS = {
+export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
   CHOOSE_OPERATION: 'choose-operation',
   CLEAR : 'clear',
@@ -11,38 +13,61 @@ const ACTIONS = {
 function reducer(state, {type, payload}){
   switch(type){
     case ACTIONS.ADD_DIGIT:
+      if(payload.digit === '0' && state.currentOperand === "0"){
+        return state
+      }
+      if(payload.digit === '.' && state.currentOperand.includes('.')){
+        return state
+      }
       return {
         ...state,
-        currentOperand: `${currentOperand || ""}${payload.digit}`
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
       }
+    case ACTIONS.CHOOSE_OPERATION:
+      if(state.currentOperand == null && state.previousOperand == null){
+        return state
+      }
+      if(state.prviousOperand == null){
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand : null,
+        }
+      }
+    case ACTIONS.CLEAR:
+        return {}
+    
+
   }
 }
 function App() {
-  const [{currentOperand, prviousOperand, operation}, dispatch] = useReducer(reducer,{});
-  dispatch({type: ACTIONS.ADD_DIGIT, payload:{digit:1}})
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer,{});
+ 
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">{prviousOperand} {operation} </div>
+        <div className="previous-operand">{previousOperand} {operation} </div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button className="span-two" onClick={()=> dispatch({type: ACTIONS.CLEAR})}>AC</button>
       <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
+      {/* <button>÷</button> */}
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
     </div>
   );
--- 01_calculator/src/DigitButton.js
+++ 01_calculator/src/DigitButton.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { ACTIONS } from './App'
 
 function DigitButton({dispatch, digit}) {
   return (
     <div>
-      
+      <button onClick={()=>dispatch({type:ACTIONS.ADD_DIGIT, payload: { digit }})}>{digit}</button>
     </div>
   )
 }
