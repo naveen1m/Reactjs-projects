11fffa32372deff3097865346c2203942f125052
naveen <stupiddint@gmail.com>
2023-02-18 18:52:59 +0530

    calculator logics

---
+++ 01_calculator/src/CalcLogic.js
@@ -0,0 +1,131 @@
+
+
+export const ACTIONS = {
+    ADD_DIGIT: 'add-digit',
+    CHOOSE_OPERATION: 'choose-operation',
+    CLEAR : 'clear',
+    DELETE_DIGIT: 'delete-digit',
+    EVALUATE: 'evaluate'
+  }
+
+function reducer(state, {type, payload}){
+    switch(type){
+      case ACTIONS.ADD_DIGIT:
+        if(state.overwrite){
+          return{
+            ...state,
+            currentOperand: payload.digit,
+            overwrite: false
+          }
+        }
+        if(payload.digit === '0' && state.currentOperand === "0"){
+          return state
+        }
+        if(payload.digit === '.' && state.currentOperand.includes('.')){
+          return state
+        }
+        return {
+          ...state,
+          currentOperand: `${state.currentOperand || ""}${payload.digit}`
+        }
+  
+      case ACTIONS.CHOOSE_OPERATION:
+        if(state.currentOperand == null && state.previousOperand == null){
+          return state
+        }
+        if(state.currentOperand == null){
+          return{
+            ...state,
+            operation: payload.operation
+          }
+        }
+        if(state.previousOperand == null){
+          return {
+            ...state,
+            operation: payload.operation,
+            previousOperand: state.currentOperand,
+            currentOperand : null,
+            
+          }
+        }
+        return{
+          ...state,
+          previousOperand : evaluate(state),
+          operation: payload.operation,
+          currentOperand: null
+        }
+  
+      case ACTIONS.CLEAR:
+          return {}
+      case ACTIONS.EVALUATE:
+        if(state.previousOperand == null && state.currentOperand != null){
+          console.log(`if case run ${state.currentOperand}`)
+          return {
+            previousOperand: null,
+            currentOperand: state.currentOperand,
+            operation: null
+          }
+        }
+        return{
+          ...state,
+          overwrite: true,
+          previousOperand: null,
+          currentOperand: evaluate(state),
+          operation: null
+        }
+  
+      case ACTIONS.DELETE_DIGIT:
+        if(state.overwrite){
+          return{
+            ...state,
+            currentOperand: null,
+            overwrite: false
+          }
+        }
+        if(state.currentOperand == null){
+          return state
+        }
+        if(state.currentOperand.length === 1){
+          return {...state, currentOperand: null}
+        }
+        return{
+          ...state,
+          currentOperand: state.currentOperand.slice(0, -1)
+        }
+    }
+  }
+  function evaluate({currentOperand, previousOperand, operation}){
+    const prev = parseFloat(previousOperand)
+    const current = parseFloat(currentOperand)
+    if(isNaN(prev) || isNaN(current)) return ""
+    let computation = ""
+    switch(operation){
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case '*':
+        computation = prev * current;
+        break;
+      case '÷':
+        computation = prev / current;
+        break;
+    }
+    return computation.toString()
+  }
+
+//   formating current and previous operand number
+  const INTEGER_FORMATTER = new Intl.NumberFormat("en-in",{
+    maximumFractionDigits: 0,
+  })
+  function formatOperand(operand){
+    if(operand == null) return
+    const [integer, decimal] = operand.split(".");
+    if(decimal == null) return INTEGER_FORMATTER.format(integer)
+    return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+  }
+
+  export default reducer
+  export {formatOperand}
