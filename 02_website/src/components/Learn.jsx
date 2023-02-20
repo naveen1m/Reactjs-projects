8c8d96426d284ed4d08efd8de47192fc9ae276ee
naveen <stupiddint@gmail.com>
2023-02-21 00:15:30 +0530

    made responsive

--- 02_website/src/components/Learn.jsx
+++ 02_website/src/components/Learn.jsx
@@ -6,11 +6,11 @@ import pm from "../assets/img/pm.jpeg"
 function Learn() {
   return (
     <>
-    <div className='max-w-[1240px] mx-auto height-[350px] bg-[#F5F4F6]  grid grid-cols-3 '>
+    <div className='max-w-[1240px] p-2 mx-auto height-[350px] bg-[#F5F4F6]  md:grid grid-cols-3 '>
         <div className='h-[250px] col-span-1 mt-3 '>
-            <img src={web} className='h-[250px] w-full ' />
+            <img src={web} alt='web development' className='h-[250px] w-[340px] mx-auto md:w-full ' />
         </div>
-        <div className='h-[250px] col-span-2 mt-3 ml-4 '>
+        <div className='h-[250px]  col-span-2 mt-3 ml-4 '>
             <h1 className='text-center text-2xl text-[#485696] font-bold '>Web Development</h1>
             <p>Web development creates websites through coding and design. It includes front-end and back-end development and aims to create user-friendly, secure, and visually appealing websites.</p>
             
@@ -18,9 +18,9 @@ function Learn() {
         
         
     </div>
-    <div className='max-w-[1240px] mx-auto height-[350px] bg-[#F5F4F6] grid grid-cols-3 '>
+    <div className='max-w-[1240px] p-2 mx-auto height-[350px] bg-[#F5F4F6] md:grid grid-cols-3 '>
         <div className='h-[250px]  col-span-1 mt-3 '>
-            <img src={ml} className='h-[250px] w-full ' />
+            <img src={ml} alt='machine learning' className='h-[250px] w-[340px] mx-auto md:w-full ' />
         </div>
         <div className='h-[250px]  col-span-2 mt-3 ml-4 '>
             <h1 className='text-center text-2xl text-[#485696] font-bold '>Machine Learning</h1>
@@ -28,9 +28,9 @@ function Learn() {
         </div>
         
     </div>
-    <div className='max-w-[1240px] mx-auto height-[350px] bg-[#F5F4F6]  grid grid-cols-3 '>
+    <div className='max-w-[1240px] p-2 mx-auto height-[350px] bg-[#F5F4F6]  md:grid grid-cols-3 '>
         <div className='h-[250px] col-span-1 mt-3 '>
-            <img src={pm} className='h-[250px] w-full ' />
+            <img src={pm} alt='project management' className='h-[250px] w-[340px] mx-auto md:w-full ' />
         </div>
         <div className='h-[250px] col-span-2 mt-3 ml-4 '>
             <h1 className='text-center text-2xl text-[#485696] font-bold '>Project Management</h1>
