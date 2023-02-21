9169853a3e4ea09e92a895c0293acc83f69ba1f9
naveen <stupiddint@gmail.com>
2023-02-21 06:45:32 +0530

    code optimized using props

--- 02_website/src/App.jsx
+++ 02_website/src/App.jsx
@@ -4,13 +4,26 @@ import './App.css';
 import Banner from './components/Banner';
 import Header from './components/Header';
 import Learn from './components/Learn';
+import webImg from './assets/img/web dev.jpeg'
+import ml from "./assets/img/ml.jpeg"
+import pm from "./assets/img/pm.jpeg"
 
+const web = 'Web Development';
+const webInfo = 'Web development creates websites through coding and design. It includes front-end and back-end development and aims to create user-friendly, secure, and visually appealing websites.'
+
+const ML = 'Machine Learning';
+const mlInfo = 'Machine learning is an AI technique that involves training algorithms to identify patterns in data and make predictions or decisions based on that data, without being explicitly programmed for each task.';
+
+const PM = 'Project Management';
+const pmInfo = 'roject management involves planning, organizing, and overseeing the execution of a project to achieve specific goals and objectives. This includes defining project requirements, allocating resources, managing timelines, and monitoring progress to ensure successful completion.'
 function App() {
   return (
     <div className='bg-[#F5F4F6]'>
       <Header />
       <Banner />
-      <Learn />
+      <Learn techName={web} info={webInfo} image={webImg} />
+      <Learn techName={ML} info={mlInfo} image={ml} />
+      <Learn techName={PM} info={pmInfo} image={pm} />
     </div>
   );
 }
--- 02_website/src/components/Learn.jsx
+++ 02_website/src/components/Learn.jsx
@@ -1,45 +1,19 @@
 import React from 'react'
-import web from "../assets/img/web dev.jpeg"
-import ml from "../assets/img/ml.jpeg"
-import pm from "../assets/img/pm.jpeg"
 
-function Learn() {
+function Learn({techName, info, image}) {
   return (
     <>
     <div className='max-w-[1240px] p-2 mx-auto height-[350px] bg-[#F5F4F6]  md:grid grid-cols-3 '>
         <div className='h-[250px] col-span-1 mt-3 '>
-            <img src={web} alt='web development' className='h-[250px] w-[340px] mx-auto md:w-full ' />
+            <img src={image} alt={techName} className='h-[250px] w-[340px] mx-auto md:w-full ' />
         </div>
         <div className='h-[250px]  col-span-2 mt-3 ml-4 '>
-            <h1 className='text-center text-2xl text-[#485696] font-bold '>Web Development</h1>
-            <p>Web development creates websites through coding and design. It includes front-end and back-end development and aims to create user-friendly, secure, and visually appealing websites.</p>
-            
-        </div>
-        
-        
+            <h1 className='text-center text-2xl text-[#485696] font-bold '>{techName}</h1>
+            <p>{info}</p>            
+        </div> 
     </div>
-    <div className='max-w-[1240px] p-2 mx-auto height-[350px] bg-[#F5F4F6] md:grid grid-cols-3 '>
-        <div className='h-[250px]  col-span-1 mt-3 '>
-            <img src={ml} alt='machine learning' className='h-[250px] w-[340px] mx-auto md:w-full ' />
-        </div>
-        <div className='h-[250px]  col-span-2 mt-3 ml-4 '>
-            <h1 className='text-center text-2xl text-[#485696] font-bold '>Machine Learning</h1>
-            <p className=' ' >Machine learning is an AI technique that involves training algorithms to identify patterns in data and make predictions or decisions based on that data, without being explicitly programmed for each task.</p>
-        </div>
-        
-    </div>
-    <div className='max-w-[1240px] p-2 mx-auto height-[350px] bg-[#F5F4F6]  md:grid grid-cols-3 '>
-        <div className='h-[250px] col-span-1 mt-3 '>
-            <img src={pm} alt='project management' className='h-[250px] w-[340px] mx-auto md:w-full ' />
-        </div>
-        <div className='h-[250px] col-span-2 mt-3 ml-4 '>
-            <h1 className='text-center text-2xl text-[#485696] font-bold '>Project Management</h1>
-            <p>roject management involves planning, organizing, and overseeing the execution of a project to achieve specific goals and objectives. This includes defining project requirements, allocating resources, managing timelines, and monitoring progress to ensure successful completion.</p>
-        </div>
-        
-    </div>
+   
     </>
   )
 }
-
 export default Learn
